293bbdd633df73e21364c75b1b0acc1534404a1c
lagrandecode <seunogunmolufirst1@gmail.com>
2023-05-01 18:24:31 +0100

    "commit"

---
+++ register/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import JsonResponse,HttpResponse
+from rest_framework.response import Response
+from rest_framework import status 
+from rest_framework.views import APIView
+
+# Create your views here.
+
+class RegisterView()
