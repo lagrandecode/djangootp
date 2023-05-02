46b0c1b5fe67bc13b38a346c7d6aba6546dec7f0
lagrandecode <seunogunmolufirst1@gmail.com>
2023-05-02 02:36:34 +0100

    "commit"

--- register/views.py
+++ register/views.py
@@ -3,6 +3,20 @@ from django.http import JsonResponse,HttpResponse
 from rest_framework.response import Response
 from rest_framework import status 
 from rest_framework.views import APIView
+from .serializers import UserSerializer
 
 # Create your views here.
 
+class RegisterView(APIView):
+    try:
+        def post(self,request):
+            serializer = UserSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    'data': serializer.data,
+                    'message':'Account created. Check your email for OTP'
+                }, status=status.HTTP_200_OK)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        print(e)
